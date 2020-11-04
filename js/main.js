f2e49287ccfdaffe9a13fffac909d1eee25fdf24
ula523 <umaksimova523@gmail.com>
2020-11-04 18:20:36 +0300

    create: added section info

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
   $('.header__button-menu').click(function(){
     $('.header__modal-dialog').toggle('header__modal-dialog--visible')
   });
+   var mainBlock = $(".main-block");
+  var mainWrapper = $(".main-wrapper");
+
+   mainBlock.on('click',function(event){
+     var activeContent = $(this).attr("data-target");
+     mainBlock.removeClass("main-block--active");
+     mainWrapper.removeClass("main-wrapper--active");
+     $(activeContent).addClass("main-wrapper--active");
+     $(this).addClass("mainBlock--active");
+   });
 })
